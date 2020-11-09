a47b4f64701c6b1d9555e8509d17584e6440b601
LOKESHCHANDRA12345 <54389664+LOKESHCHANDRA12345@users.noreply.github.com>
2020-11-10 02:35:39 +0530

    Add files via upload

---
+++ Profit and Loss.py
@@ -0,0 +1,9 @@
+CP = int(input("Enter the CP of product"))
+SP = int(input("ENTER THE SP OF PRODUCT"))
+if(CP>SP):
+    print("Profit occured")
+elif(CP<SP):
+    print("Loss Occured")
+else:
+    print("Neither P nor L")
+
